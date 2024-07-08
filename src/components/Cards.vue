ce1649fbec1cd88d87e707051ffc15237b2b3d13
BeneArp <benedetta.arpino@gmail.com>
2024-07-08 17:31:05 +0200

    feat: add mousehover show comic type and price BONUS

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -4,20 +4,33 @@
 
         props:{
             menuItems: Array,
-        }
+        },
     }
 
 </script>
 
 <template>
 
-    <div class="card" v-for="(comic, index) in menuItems" :key="index">
+    <div class="card" v-for="(comic, index) in menuItems" :key="index" @mouseenter="comic.hover = true" @mouseleave="comic.hover = false">
 
         <div class="cover">
             <img :src="comic.thumb" alt="">
         </div>
 
         <span>{{comic.series}}</span>
+
+        <div class="info" v-if="comic.hover === true ? 'hidden' : '' ">
+
+            <span>
+                Genere:
+                <h5>{{ comic.type }}</h5>
+            </span>
+            <span>
+                Prezzo:
+                <h5>{{ comic.price }}</h5>
+            </span>
+
+        </div>
 
     </div>
 
@@ -31,6 +44,7 @@
         margin: 2em 0;
         width: 15%;
         cursor: pointer;
+        position: relative;
         // debug
 
         .cover{
@@ -51,6 +65,25 @@
             color: white;
             margin-top: 1em;
             text-transform: uppercase;
+        }
+
+        .info{
+            width: 7vw;
+            height: 7vw;
+            background-color: $main-background;
+            border: 2px solid $main-nav-background;
+            position: absolute;
+            top: 10%;
+            left: 50%;
+            transform: translate(-50%);
+            opacity: 0.8;
+            padding: 1em 0;
+            font-size: 18px;
+            text-align: center;
+        }
+
+        .hidden{
+            display: none;
         }
     }
 
