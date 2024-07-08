e040342018d33ee24a305068a61c0467f6d08551
BeneArp <benedetta.arpino@gmail.com>
2024-07-08 16:28:14 +0200

    feat: add dynamic content to cards layout

--- src/components/Cards.vue
+++ src/components/Cards.vue
@@ -2,8 +2,8 @@
     export default {
         name: "Cards",
 
-        data(){
-
+        props:{
+            menuItems: Array,
         }
     }
 
@@ -11,124 +11,15 @@
 
 <template>
 
-    <div class="card">
+    <div class="card" v-for="(comic, index) in menuItems" :key="index">
 
         <div class="cover">
-            <img src="https://www.coverbrowser.com/image/action-comics/1-1.jpg" alt="">
+            <img :src="comic.thumb" alt="">
         </div>
 
-        <span>ACTION COMICS</span>
+        <span>{{comic.series}}</span>
 
     </div>
-
-    <div class="card">
-
-        <div class="cover">
-            <img src="https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg" alt="">
-        </div>
-        
-        <span>ACTION COMICS</span>
-
-    </div>
-
-    <div class="card">
-
-        <div class="cover">
-            <img src="https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg" alt="">
-        </div>
-        
-        <span>ACTION COMICS</span>
-
-    </div>
-
-    <div class="card">
-
-        <div class="cover">
-            <img src="https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg" alt="">
-        </div>
-        
-        <span>ACTION COMICS</span>
-
-    </div>
-
-    <div class="card">
-
-        <div class="cover">
-            <img src="https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg" alt="">
-        </div>
-        
-        <span>ACTION COMICS</span>
-
-    </div>
-
-    <div class="card">
-
-        <div class="cover">
-            <img src="https://static.wikia.nocookie.net/marvel_dc/images/5/50/Batman_Beyond_v.1_1.jpg" alt="">
-        </div>
-        
-        <span>ACTION COMICS</span>
-
-    </div>
-    <div class="card">
-
-<div class="cover">
-    <img src="https://www.coverbrowser.com/image/action-comics/1-1.jpg" alt="">
-</div>
-
-<span>ACTION COMICS</span>
-
-</div>
-
-<div class="card">
-
-<div class="cover">
-    <img src="https://www.panini.it/media/catalog/product/cache/a5b5dd3adfe0d321084804c738f29601/M/1/M1BLLA015ISBN_0.jpg" alt="">
-</div>
-
-<span>ACTION COMICS</span>
-
-</div>
-
-<div class="card">
-
-<div class="cover">
-    <img src="https://media.wired.com/photos/593384dad80dd005b42b2817/master/w_2560%2Cc_limit/Aquaman-16.jpg" alt="">
-</div>
-
-<span>ACTION COMICS</span>
-
-</div>
-
-<div class="card">
-
-<div class="cover">
-    <img src="https://d29xot63vimef3.cloudfront.net/image/batgirl/1-1.jpg" alt="">
-</div>
-
-<span>ACTION COMICS</span>
-
-</div>
-
-<div class="card">
-
-<div class="cover">
-    <img src="https://static.posters.cz/image/750/locandine-film-in-plexiglass-batman-prowl-comic-cover-i69653.jpg" alt="">
-</div>
-
-<span>ACTION COMICS</span>
-
-</div>
-
-<div class="card">
-
-<div class="cover">
-    <img src="https://static.wikia.nocookie.net/marvel_dc/images/5/50/Batman_Beyond_v.1_1.jpg" alt="">
-</div>
-
-<span>ACTION COMICS</span>
-
-</div>
 
 </template>
 
@@ -138,13 +29,14 @@
 
     .card{
         margin: 2em 0;
+        width: 15%;
         // debug
 
         .cover{
             background-color: lightblue;
             overflow: hidden;
             height: 9vw;
-            width: 9vw;
+            width: 100%;
 
             img{
                 display: block;
@@ -157,6 +49,7 @@
             display: block;
             color: white;
             margin-top: 1em;
+            text-transform: uppercase;
         }
     }
 
